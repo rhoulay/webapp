2358b3533f3e5e213c822ac90379766cd8022f4d
fish <diamond.zuo@gmail.com>
2015-03-12 14:49:27 +0800

    some changes

--- lovehome/app.js
+++ lovehome/app.js
@@ -1,42 +1,64 @@
 // Initialize Swiper
 $(document).ready(function () {
-    //initialize swiper when document ready
-    var mySwiper = new Swiper ('.swiper-container', {
-        // Optional parameters
-        direction: 'vertical',
-        onInit: function(swiper){
-            hideSlide();
-            showOnebyOne();
-        },
-        onTransitionEnd: function(swiper){
-            hideSlide();
-            showOnebyOne();
-        },
-        onTransitionStart: function(swiper){
-            console.log(swiper)
-        }
-    });
 
-    function hideOpacity(obj){
+    $('#logo').hide()
+    $('.swiper-container').hide()
+    $('.background').hide()
+    function overLoading() {
+        $('#loading').addClass("animated bounceOut").one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+            $("#loading").removeClass("animated bounceOut");
+            $("#loading").hide();
+            $('#logo').show();
+            $('.background').show()
+
+            $('.swiper-container').show()
+            //initialize swiper when document ready
+            var mySwiper = new Swiper('.swiper-container', {
+                // Optional parameters
+                direction: 'vertical',
+                onInit: function (swiper) {
+                    hideSlide();
+                    showOnebyOne();
+                },
+                onTransitionEnd: function (swiper) {
+                    hideSlide();
+                    showOnebyOne();
+                },
+                onTransitionStart: function (swiper) {
+                    console.log(swiper)
+                }
+            });
+            showOnebyOne()
+        });
+    }
+
+    window.setTimeout(function () {
+        overLoading();
+    }, 500);
+
+
+    function hideOpacity(obj) {
         obj.css('opacity', '0');
     }
-    function showOpacity(obj){
+
+    function showOpacity(obj) {
         obj.css('opacity', '1');
     }
 
 
-    function hideSlide(){
-        $('.swiper-slide').children().each(function(element){
-            if($(this).attr('animated-css')){
+    function hideSlide() {
+        $('.swiper-slide').children().each(function (element) {
+            if ($(this).attr('animated-css')) {
                 hideOpacity($(this));
             }
         });
     }
-    function showOnebyOne(){
-        $('.swiper-slide-active').children().each(function(element){
-            if($(this).attr('animated-css')){
+
+    function showOnebyOne() {
+        $('.swiper-slide-active').children().each(function (element) {
+            if ($(this).attr('animated-css')) {
                 showOpacity($(this));
-                $(this).addClass('animated ' + $(this).attr('animated-css')).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+                $(this).addClass('animated ' + $(this).attr('animated-css')).one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
                     $(this).removeClass('animated ' + $(this).attr('animated-css'));
                 });
             }
